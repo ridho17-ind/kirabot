e072452f2e665a3abdfa84f58f60855609c70e76
Xbarox <92025643+Xbaroxx@users.noreply.github.com>
2021-10-30 22:13:08 +0700

    Update __main__.py

--- NaoRobot/__main__.py
+++ NaoRobot/__main__.py
@@ -786,7 +786,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Yoss I'm alive ")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Yess I'm alive ")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
