c6fb57e3cdcf4d0f225f062295103c849e634f93
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-11-03 16:35:02 +0700

    Update shippering.py

--- NaoRobot/modules/shippering.py
+++ NaoRobot/modules/shippering.py
@@ -45,9 +45,9 @@ async def couple(_, message):
             c1_mention = (await app.get_users(c1_id)).mention
             c2_mention = (await app.get_users(c2_id)).mention
 
-            couple_selection_message = f"""**Couple of the day:**
+            couple_selection_message = f"""**Couple Of The Day:**
 {c1_mention} + {c2_mention} = ❤️
-__New couple of the day may be chosen at 12AM {tomorrow}__"""
+__New Couple Of The Day May Be Chosen At 12AM {tomorrow}__"""
             await app.send_message(
                 message.chat.id,
                 text=couple_selection_message
