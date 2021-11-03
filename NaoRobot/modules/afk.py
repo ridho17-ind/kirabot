be43bce875ec6a518839c6984cad90fc39d56d9c
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-11-03 16:26:00 +0700

    Update afk.py

--- NaoRobot/modules/afk.py
+++ NaoRobot/modules/afk.py
@@ -31,14 +31,14 @@ def afk(update: Update, context: CallbackContext):
         reason = args[1]
         if len(reason) > 100:
             reason = reason[:100]
-            notice = "\nYour afk reason was shortened to 100 characters."
+            notice = "\nYour Afk Reason Was Shortened To 100 Characters."
     else:
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
+        update.effective_message.reply_text("{} Sedang Offline{}".format(fname, notice))
     except BadRequest:
         pass
 
@@ -58,14 +58,14 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
+                "{} Sudah Kembali",
+                "{} Lord Sudah Online",
+                "{} Lord Di Cariin Sama Dia",
+                "{} Suhu Sudah Online",
+                "{} Yeee Crushmu Diambil Orang",
+                "{} Akhirnya Lord Online",
+                "Selamat Datang Lagi {}",
+                "Dimanakah {}?\nDi Hatimu",
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
